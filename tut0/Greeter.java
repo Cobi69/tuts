895972e72de5a82d0d6815c642a6f77b8252b831
Cobus <cobusreyneke1004@gmail.com>
2024-07-31 10:17:12 +0200

    [#12]

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -1,20 +1,15 @@
+import java.util.Scanner; 
+
 public class Greeter { 
 
-public static void main(String[] args) { 
+	public static void main(String[] args) { 
+		greeter(); 
+	} 
 
- 
-
-} 
-
-  
-
-public static String getName() { 
-
-Scanner sc = new Scanner(System.in); 
-		System.out.print(“Please provide your name: “); 
+	public static void greeter() { 
+		Scanner sc = new Scanner(System.in); 
+		System.out.print("Please provide your name: "); 
 		String name = sc.nextLine(); 
-		return name;		 
-
-} 
-
+		System.out.println("Hello there, " + name + ". How are you doing?"); 
+	} 
 }
